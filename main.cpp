3ca6ce15b217c03361d0140173d3173589dec54f
Daniel Pilich <dpilich16@gmail.com>
2025-04-01 14:54:22 +0200

    feat: Add movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h";
+#include "raymath.h";
 
 int main(){
     const int windowDimensions[2]{
@@ -6,17 +7,29 @@ int main(){
         384 // height
     };
 
+    const float speed{4.0};
+
     InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash!");
 
     Texture2D map = LoadTexture("assets/map/WorldMap.png");
+    Vector2 mapPosition{0.0, 0.0};
     
     SetTargetFPS(60);
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 mapPosition{0.0, 0.0};
-        DrawTextureEx(map, mapPosition, 0.0, 2.0, WHITE);
+        Vector2 direction{};
+        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if(IsKeyDown(KEY_D)) direction.x += 1.0;
+        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if(IsKeyDown(KEY_S)) direction.y += 1.0;
+        if(Vector2Length(direction) != 0.0){
+            Vector2 movement = Vector2Scale(Vector2Normalize(direction), speed);
+            mapPosition = Vector2Subtract(mapPosition, movement); // mapPosition = mapPosition - movement
+        }
+
+        DrawTextureEx(map, mapPosition, 0.0, 4.0, WHITE);
 
         EndDrawing();
     }
