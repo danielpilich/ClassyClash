8b2d555ef54a432c194e22d2c509340b310662a1
Daniel Pilich <dpilich16@gmail.com>
2025-04-01 16:03:41 +0200

    feat: Draw the hero texture

--- main.cpp
+++ main.cpp
@@ -1,35 +1,60 @@
 #include "raylib.h";
 #include "raymath.h";
 
-int main(){
-    const int windowDimensions[2]{
-        384, // width
-        384 // height
-    };
+int main()
+{
+    const int windowWidth = 384;
+    const int windowHeight = 384;
 
     const float speed{4.0};
 
-    InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash!");
+    InitWindow(windowWidth, windowHeight, "Classy Clash!");
 
     Texture2D map = LoadTexture("assets/map/WorldMap.png");
     Vector2 mapPosition{0.0, 0.0};
-    
+
+    Texture2D hero = LoadTexture("assets/characters/hero_idle.png");
+    Vector2 heroPosition{
+        (float)windowWidth / 2.0f - 4.0f * (0.5f * (float)hero.width / 6.0f),
+        (float)windowHeight / 2.0f - 4.0f * (0.5f * (float)hero.height)};
+
     SetTargetFPS(60);
-    while(!WindowShouldClose()){
+
+    while (!WindowShouldClose())
+    {
         BeginDrawing();
         ClearBackground(WHITE);
 
         Vector2 direction{};
-        if(IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if(IsKeyDown(KEY_D)) direction.x += 1.0;
-        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if(IsKeyDown(KEY_S)) direction.y += 1.0;
-        if(Vector2Length(direction) != 0.0){
+        if (IsKeyDown(KEY_A))
+            direction.x -= 1.0;
+        if (IsKeyDown(KEY_D))
+            direction.x += 1.0;
+        if (IsKeyDown(KEY_W))
+            direction.y -= 1.0;
+        if (IsKeyDown(KEY_S))
+            direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
             Vector2 movement = Vector2Scale(Vector2Normalize(direction), speed);
             mapPosition = Vector2Subtract(mapPosition, movement); // mapPosition = mapPosition - movement
         }
 
+        // Draw the map
         DrawTextureEx(map, mapPosition, 0.0, 4.0, WHITE);
+
+        // Draw the character
+        Rectangle heroSource{
+            0.f,
+            0.f,
+            (float)hero.width / 6.f,
+            (float)hero.height};
+        Rectangle heroDest{
+            heroPosition.x,
+            heroPosition.y,
+            4.f * hero.width / 6.f,
+            4.f * hero.height};
+        DrawTexturePro(hero, heroSource, heroDest, Vector2{}, 0.f, WHITE);
 
         EndDrawing();
     }
